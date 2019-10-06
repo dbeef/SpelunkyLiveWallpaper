9aec0c15e3b592c6a8f86e456578b886345da8a9
dbeef <daniel.zalega@gmail.com>
2019-10-06 13:17:24 +0200

    Small optimizations

--- core/src/com/mygdx/game/level/LevelRenderer.java
+++ core/src/com/mygdx/game/level/LevelRenderer.java
@@ -2,15 +2,12 @@ package com.mygdx.game.level;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.Assets;
-import com.mygdx.game.Consts;
 
-import static com.badlogic.gdx.Gdx.gl;
 import static com.mygdx.game.Consts.*;
 import static com.mygdx.game.Consts.SCREEN_HEIGHT;
 import static com.mygdx.game.level.Level.TILES_X;
@@ -20,67 +17,16 @@ public class LevelRenderer {
 
     public LevelRenderer(Assets assets) {
         this.assets = assets;
+
         camera = new OrthographicCamera(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
         camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
         batcher = new SpriteBatch();
-
         cameraVelocity = new Vector2(0.5f, 0.5f);
-    }
 
-    public boolean tileInViewport(int x, int y)
-    {
-        if (x > camera.position.x - (0.5 * camera.viewportWidth) - TILE_WIDTH &&
-                x < camera.position.x + (0.5 * camera.viewportWidth) &&
-                y > camera.position.y - (0.5 * camera.viewportHeight) - TILE_HEIGHT &&
-                y < camera.position.y + (0.5 * camera.viewportHeight)
-        )
-            return true;
-        else
-            return false;
-    }
-
-    public boolean viewportXOutOfLevel() {
-        return (camera.position.x + (0.5f * camera.viewportWidth) > TILE_WIDTH * TILES_X) ||
-                camera.position.x < camera.viewportWidth / 2;
-    }
-
-    public boolean viewportYOutOfLevel() {
-        return (camera.position.y + (0.5f * camera.viewportHeight) > TILE_HEIGHT * TILES_Y) ||
-                camera.position.y < camera.viewportHeight / 2;
-    }
-
-    public void floatCamera(float delta) {
-        cameraMoveTimer += delta;
-
-        if(cameraMoveTimer > (1.0f / 30.0f))
-        {
-            cameraMoveTimer = 0;
-            camera.translate(cameraVelocity);
-            if(viewportXOutOfLevel())
-                cameraVelocity.x *= -1;
-            if(viewportYOutOfLevel())
-                cameraVelocity.y *= -1;
-        }
-    }
-
-    // For debugging.
-    public void takeInput() {
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            camera.translate(-2f, 0, 0);
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            camera.translate(+2f, 0, 0);
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            camera.translate(0, -2, 0);
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            camera.translate(0, +2, 0);
-        }
+        camera.update();
     }
 
     public void draw(Level level) {
-        camera.update();
         batcher.setProjectionMatrix(camera.combined);
         batcher.begin();
 
@@ -105,9 +51,62 @@ public class LevelRenderer {
         batcher.end();
     }
 
-    OrthographicCamera camera;
-    SpriteBatch batcher;
-    Assets assets;
-    float cameraMoveTimer = 0;
-    Vector2 cameraVelocity;
+    private boolean tileInViewport(int x, int y) {
+        if (x > camera.position.x - (0.5 * camera.viewportWidth) - TILE_WIDTH &&
+                x < camera.position.x + (0.5 * camera.viewportWidth) &&
+                y > camera.position.y - (0.5 * camera.viewportHeight) - TILE_HEIGHT &&
+                y < camera.position.y + (0.5 * camera.viewportHeight)
+        )
+            return true;
+        else
+            return false;
+    }
+
+    private boolean viewportXOutOfLevel() {
+        return (camera.position.x + (0.5f * camera.viewportWidth) > TILE_WIDTH * TILES_X) ||
+                camera.position.x < camera.viewportWidth / 2;
+    }
+
+    public boolean viewportYOutOfLevel() {
+        return (camera.position.y + (0.5f * camera.viewportHeight) > TILE_HEIGHT * TILES_Y) ||
+                camera.position.y < camera.viewportHeight / 2;
+    }
+
+    public void floatCamera(float delta) {
+        cameraMoveTimer += delta;
+
+        if(cameraMoveTimer > (1.0f / 30.0f))
+        {
+            cameraMoveTimer = 0;
+            camera.translate(cameraVelocity);
+            if(viewportXOutOfLevel())
+                cameraVelocity.x *= -1;
+            if(viewportYOutOfLevel())
+                cameraVelocity.y *= -1;
+
+            camera.update();
+        }
+    }
+
+    // For debugging.
+    public void takeInput() {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            camera.translate(-2f, 0, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            camera.translate(+2f, 0, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            camera.translate(0, -2, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            camera.translate(0, +2, 0);
+        }
+    }
+
+    private OrthographicCamera camera;
+    private SpriteBatch batcher;
+    private Assets assets;
+    private float cameraMoveTimer = 0;
+    private Vector2 cameraVelocity;
 }
